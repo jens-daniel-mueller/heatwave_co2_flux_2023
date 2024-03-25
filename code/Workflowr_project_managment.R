25ae26997885821b395377eb189cfedc9e3c7d24
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2024-03-25 14:41:49 +0100

    Daily MHW stats added

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -120,7 +120,7 @@ wflow_publish(here::here(
 message = "use setup child Rmd file")
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "MHW stats started")
+wflow_publish(all = TRUE, message = "Daily MHW stats added")
 
 # commit changes including _site.yml (locally) and rebuild site
 wflow_publish(c("analysis/*Rmd"), message = "include G19 comparison", republish = TRUE)
